46cb91c0c98bf44f6f7bed023e04489a3d12271e
JUNYI <pengjunyi.cn@gmail.com>
2018-10-03 17:52:12 -0400

    rasing event

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,20 +2,72 @@ import React, { Component } from 'react'
 
 class Counter extends Component {
   state = {
-    count: 1,
-    formatCount: this.formatCount.bind(this)
+    value: this.props.counter.value,
+    // imgUrl: 'https://picsum.photos/200',
+    tags: []
   }
-  formatCount() {
-    const { count } = this.state
-    return count === 0 ? 'Zero' : count
+  //   constructor() {
+  //     super()
+  //     this.handleIncrement = this.handleIncrement.bind(this)
+  //   }
+  //   style = {
+  //     fontSize: 10,
+  //     fontWeight: 'bold'
+  //   }
+
+  //   renderTags() {
+  //     if (this.state.tags.length === 0) {
+  //       return <p>There are no tags!</p>
+  //     }
+  //     return (
+  //       <ul>
+  //         {this.state.tags.map((tag, i) => (
+  //           <li key={i}>{tag}</li>
+  //         ))}
+  //       </ul>
+  //     )
+  //   }
+  handleIncrement() {
+    // console.log(product)
+    this.setState({ value: this.state.value + 1 })
   }
+
+  // Arrow Function can avoid bind and assign this to this function
+  //   handleIncrement = () => {
+  //     console.log('Increment Clicked', this)
+  //   }
   render() {
     return (
-      <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
-      </React.Fragment>
+      <div>
+        {/* <img src={this.state.imgUrl} alt="testImage" /> */}
+        <h4>Counter# {this.props.counter.id}</h4>
+        <span className={this.getBadgeClass()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement()}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {/* {this.state.tags.length === 0 && 'Please add a tag'}
+        {this.renderTags()} */}
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger m-2"
+        >
+          Delete
+        </button>
+      </div>
     )
+  }
+  getBadgeClass() {
+    let classes = 'badge m-2 badge-'
+    classes += this.state.value === 0 ? 'warning' : 'primary'
+    return classes
+  }
+
+  formatCount() {
+    const { value } = this.state
+    return value === 0 ? 'Zero' : value
   }
 }
 
