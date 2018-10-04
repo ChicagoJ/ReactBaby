220e0086a2f07470167642fc47d7cb4fa635d640
JUNYI <pengjunyi.cn@gmail.com>
2018-10-04 13:56:27 -0400

    change bind function to constructor

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import './App.css';
-import NavBar from './components/NavBar';
-import Counters from './components/counters';
+import React, { Component } from 'react'
+import './App.css'
+import NavBar from './components/NavBar'
+import Counters from './components/counters'
 
 class App extends Component {
   state = {
@@ -11,38 +11,44 @@ class App extends Component {
       { id: 3, value: 0 },
       { id: 4, value: 0 }
     ]
-  };
-  componentWillMount() {
-    console.log('App - will mount');
   }
+
   constructor() {
-    super();
-    console.log('App - contructed');
+    super()
+    this.handleIncrement = this.handleIncrement.bind(this)
+    this.handleDelete = this.handleDelete.bind(this)
+    this.handleReset = this.handleReset.bind(this)
+    console.log('App - contructed')
   }
+
+  componentWillMount() {
+    console.log('App - will mount')
+  }
+
   componentDidMount() {
-    console.log('App - Mounted');
+    console.log('App - Mounted')
   }
 
   handleIncrement(counter) {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    counters[index].value++;
-    this.setState({ counters });
+    const counters = [...this.state.counters]
+    const index = counters.indexOf(counter)
+    counters[index] = { ...counter }
+    counters[index].value++
+    this.setState({ counters })
   }
   handleDelete(id) {
-    const counters = this.state.counters.filter(c => c.id !== id);
-    this.setState({ counters });
+    const counters = this.state.counters.filter(c => c.id !== id)
+    this.setState({ counters })
   }
   handleReset() {
     const counters = this.state.counters.map(c => {
-      c.value = 0;
-      return c;
-    });
-    this.setState({ counters });
+      c.value = 0
+      return c
+    })
+    this.setState({ counters })
   }
   render() {
-    console.log('App - rendered');
+    console.log('App - rendered')
 
     return (
       <React.Fragment>
@@ -52,14 +58,14 @@ class App extends Component {
         <main role="main" className="container">
           <Counters
             counters={this.state.counters}
-            onIncrement={this.handleIncrement.bind(this)}
-            onDelete={this.handleDelete.bind(this)}
-            onReset={this.handleReset.bind(this)}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+            onReset={this.handleReset}
           />
         </main>
       </React.Fragment>
-    );
+    )
   }
 }
 
-export default App;
+export default App
