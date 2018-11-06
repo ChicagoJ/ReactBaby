360dc8ac9abf2d4c4597401a726ae10578926b13
JUNYI <pengjunyi.cn@gmail.com>
2018-11-06 11:33:19 -0500

    intro Bootstrap

--- zhufengReactStudy/201811reactrouter/src/components/App.js
+++ zhufengReactStudy/201811reactrouter/src/components/App.js
@@ -1,18 +1,43 @@
 import React from 'react'
 import {
   HashRouter as Router, //容器
-  Route //一条路由
+  Route, //一条路由
+  Link
 } from 'react-router-dom'
 import Home from './Home'
 import User from './User'
 import Profile from './Profile'
-
+import 'bootstrap/dist/css/bootstrap.css'
 export default (
   <Router>
     <div>
-      <Route path="/home" component={Home} />
-      <Route path="/User" component={User} />
-      <Route path="/Profile" component={Profile} />
+      <nav className="navbar navbar-inverse">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <div className="navbar-brand">User Manager System</div>
+          </div>
+          <ul className="nav navbar-nav">
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="/user">User Setting</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12">
+            <Route path="/home" component={Home} />
+            <Route path="/User" component={User} />
+            <Route path="/Profile" component={Profile} />
+          </div>
+        </div>
+      </div>
     </div>
   </Router>
 )
