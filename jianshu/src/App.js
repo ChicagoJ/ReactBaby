51fcced0b339506f11267c38dceac37cf77825d2
JUNYI <pengjunyi.cn@gmail.com>
2018-11-13 18:09:22 -0500

    initial

---
+++ jianshu/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+import { AppWrapper } from './style.js'
+class App extends Component {
+  render() {
+    return (
+      <AppWrapper>
+        <div className="App">Hello World</div>
+      </AppWrapper>
+    )
+  }
+}
+
+export default App
