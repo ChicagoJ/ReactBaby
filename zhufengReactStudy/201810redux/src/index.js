bcfd668e5f4a4dc54161d7e65ca85aae3a168231
JUNYI <pengjunyi.cn@gmail.com>
2018-11-24 14:36:30 -0500

    updatedfrommac

--- zhufengReactStudy/201810redux/src/index.js
+++ zhufengReactStudy/201810redux/src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom'
 import Counter from './components/Counter2'
 import store from './store2'
 import Provider from './components/Provider'
-import Todo from './components/Todo'
+// import Todo from './components/Todo'
 ReactDOM.render(
-  // <Provider store={store}>
-  //   <Counter />
-  // </Provider>,
-  <Todo />,
+  <Provider store={store}>
+    <Counter />
+  </Provider>,
+
   document.querySelector('#root')
 )
